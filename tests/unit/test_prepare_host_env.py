7fc7622dea913ec590cd6a59c6c868e8d3e69494
misohu <michal.hucko@canonical.com>
2024-01-22 10:37:24 +0100

    Debug

--- tests/unit/test_prepare_host_env.py
+++ tests/unit/test_prepare_host_env.py
@@ -1,11 +1,16 @@
 import pytest
+
 from dss.prepare_host_env import generate_prepare_host_script
 
-@pytest.mark.parametrize("gpu_driver_version, expected_line", [
-    ("auto", "sudo microk8s enable gpu --driver auto"),
-    ("none", ""),
-    ("535.129.03", "sudo microk8s enable gpu --set driver.version='535.129.03'"),
-])
+
+@pytest.mark.parametrize(
+    "gpu_driver_version, expected_line",
+    [
+        ("auto", "sudo microk8s enable gpu --driver auto"),
+        ("none", ""),
+        ("535.129.03", "sudo microk8s enable gpu --set driver.version='535.129.03'"),
+    ],
+)
 def test_generate_prepare_host_script(gpu_driver_version, expected_line):
     result = generate_prepare_host_script(gpu_driver_version)
 
