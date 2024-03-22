749e71cdf265b9fd0f1083b3934baeeec2484a60
NohaIhab <noha.ihab@canonical.com>
2024-03-22 10:23:05 +0000

    fix unit test and add test for bad scenario

--- tests/unit/test_remove_notebook.py
+++ tests/unit/test_remove_notebook.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock, patch
 
 import pytest
+from charmed_kubeflow_chisme.lightkube.mocking import FakeApiError
 
 from dss.config import DSS_NAMESPACE
 from dss.remove_notebook import remove_notebook
@@ -33,16 +34,28 @@ def test_remove_notebook_success(
     """
     notebook_name = "test-notebook"
 
-    # Mock wait_for_deployment_ready
-    with patch(
-        "dss.remove_notebook.wait_for_deployment_deleted"
-    ) as mock_wait_for_deployment_deleted:
-        # Call the function to test
-        remove_notebook(name=notebook_name, lightkube_client=mock_client)
+    # Call the function to test
+    remove_notebook(name=notebook_name, lightkube_client=mock_client)
 
-        # Assertions
-        mock_client.delete.call_count == 2
-        mock_wait_for_deployment_deleted.assert_called_once_with(
-            mock_client, namespace=DSS_NAMESPACE, deployment_name=notebook_name
-        )
-        mock_logger.info.assert_called_with(f"Notebook {notebook_name} removed.")
+    # Assertions
+    mock_client.delete.call_count == 2
+    mock_logger.info.assert_called_with(f"Notebook {notebook_name} removed.")
+
+
+def test_remove_notebook_not_found(
+    mock_client: MagicMock,
+    mock_logger: MagicMock,
+) -> None:
+    """
+    Tests case to verify failed remove-notebook call with 404 error.
+    """
+    notebook_name = "test-notebook"
+
+    mock_client.delete.side_effect = FakeApiError(404)
+
+    # Call the function to test
+    remove_notebook(name=notebook_name, lightkube_client=mock_client)
+
+    mock_logger.warn.assert_called_with(
+        "Failed to delete K8s resources not found. Ignoring remove-notebook."
+    )
