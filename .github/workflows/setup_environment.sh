6aa5e4ee3317200a9ee272d8aee207265f2683af
misohu <michal.hucko@canonical.com>
2024-04-30 08:35:47 +0200

    Implement gpu tox integration

--- .github/workflows/setup_environment.sh
+++ .github/workflows/setup_environment.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ensure the script is running as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
 # Python setup
 apt-get update -yqq
 apt-get install -yqq python3-pip
@@ -7,7 +13,5 @@ pip3 install tox
 
 # Microk8s setup
 snap install microk8s --channel=1.28/stable --classic
-usermod -a -G microk8s $USER
-mkdir -p /home/$USER/.kube
-chown -f -R $USER /home/$USER/.kube
+usermod -a -G microk8s ubuntu
 newgrp microk8s
--- .github/workflows/setup_microk8s.sh
+++ .github/workflows/setup_microk8s.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# This needs to be a separate file as newgrp will not have effect from  setup_environment.sh
+# This needs to be a separate file as newgrp will not have effect from setup_environment.sh
 microk8s enable storage dns rbac gpu
 microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
 
 # Install + setup kubectl
 snap install kubectl --classic
-microk8s config > /home/$USER/.kube/config
-chmod 777 /home/$USER/.kube/config
+microk8s config > ~/.kube/config
+chown ubuntu:ubuntu ~/.kube/config
+chmod 600 ~/.kube/config
