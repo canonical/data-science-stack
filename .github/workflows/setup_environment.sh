1b0c4137ceab0330440e285022895710f7a3de86
misohu <michal.hucko@canonical.com>
2024-04-30 08:22:34 +0200

    Implement gpu tox integration

--- .github/workflows/setup_environment.sh
+++ .github/workflows/setup_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Python setup
-sudo apt-get update -yqq
-sudo apt-get install -yqq python3-pip
-sudo --preserve-env=http_proxy,https_proxy,no_proxy pip3 install tox
+apt-get update -yqq
+apt-get install -yqq python3-pip
+pip3 install tox
 
 # Microk8s setup
-sudo snap install microk8s --channel=1.28/stable --classic
-sudo usermod -a -G microk8s ubuntu
-mkdir -p /home/ubuntu/.kube
-sudo chown -f -R ubuntu /home/ubuntu/.kube
+snap install microk8s --channel=1.28/stable --classic
+usermod -a -G microk8s $USER
+mkdir -p /home/$USER/.kube
+chown -f -R $USER /home/$USER/.kube
 newgrp microk8s
--- .github/workflows/setup_microk8s.sh
+++ .github/workflows/setup_microk8s.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # This needs to be a separate file as newgrp will not have effect from  setup_environment.sh
-sudo microk8s enable storage dns rbac gpu
+microk8s enable storage dns rbac gpu
 microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
 
 # Install + setup kubectl
-sudo snap install kubectl --classic
-microk8s config > /home/ubuntu/.kube/config
-chmod 777 /home/ubuntu/.kube/config
+snap install kubectl --classic
+microk8s config > /home/$USER/.kube/config
+chmod 777 /home/$USER/.kube/config
