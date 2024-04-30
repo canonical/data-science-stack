1bffd5405c8108c6eacecdb88385a1a970243d46
misohu <michal.hucko@canonical.com>
2024-04-30 10:20:02 +0200

    Increase integration tests timeout

--- .github/workflows/setup_microk8s.sh
+++ .github/workflows/setup_microk8s.sh
@@ -4,6 +4,13 @@
 microk8s enable storage dns rbac gpu
 microk8s kubectl rollout status deployment/hostpath-provisioner -n kube-system
 
+# Wait for gpu operator components to be ready
+while ! sudo microk8s.kubectl logs -n gpu-operator-resources -l app=nvidia-operator-validator | grep "all validations are successful"
+do
+  echo "waiting for validations"
+  sleep 5
+done
+
 # Setup config
 microk8s config > ~/.kube/config
 chown ubuntu:ubuntu ~/.kube/config
